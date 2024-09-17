4012b9d325ce10e28e5f7da54bfe53e21da599e2
Karthikeyan S <karthik0x00@gmail.com>
2024-09-17 16:31:59 +0530

    Initial commit

---
+++ src/main/java/com/challenge/PennyPilot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.challenge.PennyPilot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PennyPilotApplication.class);
+	}
+
+}
---
+++ src/test/java/com/challenge/PennyPilot/PennyPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.PennyPilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennyPilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
